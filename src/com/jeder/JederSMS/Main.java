b6b0c440a9dd1cfb36e74e818340c8b39d090a07
ostrichbeta <ostrichb@outook.com>
2018-02-09 09:46:06 +0000

    增入了一些內容

--- src/com/jeder/JederSMS/Main.java
+++ src/com/jeder/JederSMS/Main.java
@@ -1,14 +1,19 @@
 package com.jeder.JederSMS;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.jeder.JederSMS.cmds.*;
 
 public class Main extends JavaPlugin {
+
+    FileConfiguration fc = getConfig() ;
 
     @Override
     public void onEnable () {
         getLogger().info( "Jeder簡訊載入中" ) ;
         //設定導出
         getCommand( "jederSMS" ).setExecutor( new SMScommands( this ) );
+
     }
+
 
 }
--- src/com/jeder/JederSMS/cmds/SMScommands.java
+++ src/com/jeder/JederSMS/cmds/SMScommands.java
@@ -3,7 +3,7 @@ import com.jeder.JederSMS.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
+import java.util.Random;
 
 public class SMScommands implements CommandExecutor {
 
@@ -21,8 +21,50 @@ public class SMScommands implements CommandExecutor {
             sender.sendMessage("I don't know what do you want to do.");
             return false;
         }
+
         if ( args[0].toLowerCase().equals( "info" ) ) {
-            return false;
+            //查詢插件訊息
+            sender.sendMessage( "----------=插件訊息提示=----------" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            sender.sendMessage( "" );
+            return true;
+        }
+
+        if ( args[0].toLowerCase().equals( "send" ) ) {
+            if ( args.length <= 1 ) {
+                //僅輸入/sms send 或沒有輸入任何訊息
+                sender.sendMessage("/sms send <玩家> <訊息>");
+                return true;
+            }
+            else {
+                //要求達到標準時
+                //生成一個訊息號
+                Random r = new Random() ;
+                int smsCode ;
+                while ( 1 == 1 ) {
+                    smsCode = r.nextInt(8999999) + 1000000;
+                    //判斷是否這個訊息號被佔用
+                    if ( plugin.getConfig().getString( "Messages." + String.valueOf( smsCode ) ).isEmpty() ) {
+                        //字串為空時
+                        //沒有異常發生
+                        plugin.getLogger().info( "§e嘗試使用§b§l " + String.valueOf( smsCode ) + " §e作為簡訊號是可以的 " );
+
+                        break;
+                    }
+                    plugin.getLogger().info( "§e嘗試使用§b§l " + String.valueOf( smsCode ) + " §e作為簡訊號時被占用，將重新生成一個 " ); ;
+                }
+                //開始設定訊息
+                plugin.getConfig().set( "Messages." + String.valueOf( smsCode ) + ".sender" , sender.getName() );
+                plugin.getConfig().set( "Messages." + String.valueOf( smsCode ) + ".reciever" , args[1] );
+                plugin.getConfig().set( "Messages." + String.valueOf( smsCode ) + ".messages" , args[2] );
+                return true;
+            }
         }
         return false;
     }
